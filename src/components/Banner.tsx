f975edb04c266e760ac59bb8575b91fe6196af97
Supun Devendra <supundevendra1207@gmail.com>
2025-01-26 15:08:45 +0530

    Add Home page and Banner component with particle effects

---
+++ src/components/Banner.tsx
@@ -0,0 +1,96 @@
+import { useCallback } from "react";
+import type { Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+const Design = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    return await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async () => {
+    },
+    []
+  );
+  return (
+    <div className="w-full h-full bg-hero-image bg-cover relative">
+      <Particles
+        className="w-full h-full"
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 60,
+          fullScreen: false,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 120,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 90,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Design;
